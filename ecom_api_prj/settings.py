8c65a95dce59d714afb8cf3185c988c1bbff5026
shubham <spchavhan32@gmail.com>
2020-10-07 02:23:08 +0530

    make it better

--- ecom_api_prj/settings.py
+++ ecom_api_prj/settings.py
@@ -22,13 +22,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecom_api_prj.urls'
@@ -116,4 +116,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
