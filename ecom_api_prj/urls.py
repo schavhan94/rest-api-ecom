822c3681f7b28c26f3a9a9825e1006f05b2a6422
shubham <spchavhan32@gmail.com>
2020-10-07 00:41:03 +0530

    initial commit

--- ecom_api_prj/urls.py
+++ ecom_api_prj/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from ecom_api_app import views
 from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register('userrole', views.UserRoleViewSet)
@@ -17,4 +17,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('login/', views.AuthToken.as_view(), name='login'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
