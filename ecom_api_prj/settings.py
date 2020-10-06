43c602c766668569b7a814b961732bf8773d2e93
shubham <spchavhan32@gmail.com>
2020-10-07 02:21:21 +0530

    make it better

--- ecom_api_prj/settings.py
+++ ecom_api_prj/settings.py
@@ -107,7 +107,10 @@ ACCOUNT_USERNAME_REQUIRED = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
