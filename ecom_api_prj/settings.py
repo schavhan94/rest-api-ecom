ad95f30208669f3deb24f8cbc09ee2685a9fe778
shubham <spchavhan32@gmail.com>
2020-10-07 11:13:25 +0530

    make it better

--- ecom_api_prj/settings.py
+++ ecom_api_prj/settings.py
@@ -102,18 +102,12 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-# STATICFILES_DIRS = (
+# STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
-# )
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
